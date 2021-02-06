008d08835617785f02b364f32c974e62ff00ca10
Terence Ateya <ateyaterence@gmail.com>
2021-02-06 12:44:58 -0600

    next commit

---
+++ SADProject1/src/KWIC/DataSink.java
@@ -0,0 +1,11 @@
+package KWIC;
+
+public class DataSink implements Runnable {
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
--- SADProject1/src/KWIC/Pipeline.java
+++ SADProject1/src/KWIC/Pipeline.java
@@ -1,5 +1,48 @@
 package KWIC;
 
+import java.util.ArrayList;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 public class Pipeline {
 
+	public static long startTime = 0;
+	public static long arrivalTime = 0;
+
+	private DataSource dataSource;
+	private DataSink dataSink;
+	private ArrayList<Filter> filters;
+	
+	private ExecutorService executorService;
+	
+	public Pipeline() {
+		this.filters = new ArrayList<>();
+	}
+
+	public Pipeline  generateFrom(DataSource dataSource) {
+		this.dataSource = dataSource;
+		
+		return this;
+	}
+	
+	public Pipeline transformBy(Filter filter) {
+		filters.add(filter);
+		return this;
+	}
+	
+	public Pipeline outputInto(DataSink dataSink) {
+		this.dataSink = dataSink;
+		return this;
+	}
+	
+	
+	public void run() {
+		executorService = Executors.newCachedThreadPool();
+		
+		executorService.execute(dataSource);
+		filters.forEach(executorService::execute);
+		executorService.execute(dataSink);
+	}
+	
+	
 }
